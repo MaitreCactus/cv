ef89d42a497969b91c375b8cded3c1168b9ed89d
MaitreCactus <malik-khelfa@hotmail.fr>
2017-02-20 10:54:31 +0100

    cv

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="cvenligne.css">
+	<meta charset="utf-8">
+	<link href="https://fonts.googleapis.com/css?family=Viga" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Black+Ops+One" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css">
+	
+
+</head>
+<body>
+<header>
+	
+		
+		<h1>Développeur-Web Junior</h1>
+		<h2>|Malik Khelfa| </h2>
+		<nav>
+			<ul>
+				<li><a href="#Qui">Qui suis-je ?</a></li>
+				<li><a href="#expe">Expériences Pro</a></li>
+				<li><a href="#Comp">Compétences</a></li>
+				<li><a href="#slideshow">Porfolio</a></li>
+				<li><a href="#cont">Contact</a></li>
+			</ul>
+		</nav>
+</header>
+
+	<div id="Qui">
+
+	<p>Issu de la branche du commerce et aprés deux ans d'expériences professionnels.<br> J'ai fais le choix de changer de profession,<br> pour me tourner vers un secteur d'avenir, le numérique.<br>J'ai par la suite intégré 'Upto', labelisée grande école du numérique.
+	<br>Une formation pour apprendre le développement-Web<br>avec pour apprentissage une méthode innovante basée sur 90% de pratique.<br>Je souhaite me spécialiser dans le développement-Mobile (Ionic, Cordova).<br>
+
+
+	</p>
+		</div>
+		<div id="deux">
+			<div id="expe">
+			<h3>|Expériences Professionnelles|</h3>
+			<ul>
+				<li>2016/2017: Formation développeur-Web</li>
+				<li>2015/2016: Recruteur de donateur</li>
+					<p>Esprit d'équipe, persévérence</p>
+				<li>2013/2015: Commercial</li>
+					<p>Technique de vente, autonomie</p>
+				<li>2012/2013: Téléconseiller</li>
+					<p>Capacité d'écouté</p>
+			</ul>
+				</div>
+					<div id="Comp">
+					<h3>|Langages en cours|</h3>
+						<img src="html5-couleur.png">
+						<img src="css3-couleur.png">
+						<img src="JS-couleur.png">
+						<img id="angu" src="angular-js-color.png">
+						<img id="symp" src="symphony-couleur.png">
+						<img src="http://upto-numerique.fr/img/langages/php-couleur.png">
+							
+							
+						</div>
+								</div>
+								
+								<div id="preslider">
+								<h3>Quelques créations</h3>
+								<span id="sl_play" class="sl_command"></span>
+<span id="sl_pause" class="sl_command"></span>
+ 
+<span id="sl_i1" class="sl_command sl_i"></span>
+<span id="sl_i2" class="sl_command sl_i"></span>
+<span id="sl_i3" class="sl_command sl_i"></span>
+<span id="sl_i4" class="sl_command sl_i"></span>
+							
+							<section id="slideshow">
+		<a class="play_commands pause" href="#sl_pause" title="Maintain paused">Pause</a>
+<a class="play_commands play" href="#sl_play" title="Play the animation">Play</a>
+<a class="commands prev commands1" href="#sl_i4" title="Aller à la dernière slide">&lt;</a>
+<a class="commands next commands1" href="#sl_i2" title="Aller à la 2ème slide">&gt;</a>
+<a class="commands prev commands2" href="#sl_i1" title="Aller à la 1ère slide">&lt;</a>
+<a class="commands next commands2" href="#sl_i3" title="Aller à la 3ème slide">&gt;</a>
+<a class="commands prev commands3" href="#sl_i2" title="Aller à la 2ème slide">&lt;</a>
+<a class="commands next commands3" href="#sl_i4" title="Aller à la 4ème slide">&gt;</a>
+<a class="commands prev commands4" href="#sl_i3" title="Aller à la 3ème slide">&lt;</a>
+<a class="commands next commands4" href="#sl_i1" title="Aller à la 1ère slide">&gt;</a>
+	<div class="container">
+		<div class="c_slider"></div>
+		<div class="slider">
+			<figure>
+				<img src="screenshot.png" width="640" height="310" />
+				<figcaption>1er Mois</figcaption>
+
+			</figure><!--
+			--><figure>
+				<img src="screenshots/player-closed.png" width="640" height="310" />
+				<figcaption>Deuxième Mois </figcaption>
+			</figure><!--
+			--><figure>
+				<img src="maquette.png" width="640" height="310" />
+				<figcaption>Troisième Mois</figcaption>
+
+			</figure><!--
+			--><figure>
+				<img src="maquette.png" width="640" height="310" />
+				<figcaption>Quatrième Mois</figcaption>
+			</figure>
+		</div>
+	</div>
+		
+	<span id="timeline"></span>
+	<ul class="dots_commands"><!--
+	--><li><a title="Afficher la slide 1" href="#sl_i1">Slide 1</a></li><!--
+	--><li><a title="Afficher la slide 2" href="#sl_i2">Slide 2</a></li><!--
+	--><li><a title="Afficher la slide 3" href="#sl_i3">Slide 3</a></li><!--
+	--><li><a title="Afficher la slide 4" href="#sl_i4">Slide 4</a></li>
+</ul>
+</section>
+</div>
+		<div id="footer">
+
+		<div id="cont">
+		<h4>En Collaboration</h4>
+		<img src="AAEAAQAAAAAAAALKAAAAJDJiNTQ1NjY4LWIyMjEtNGU3Yi1hYmYxLTFlMmExNDM1OGVhMw.png">
+		<img src="french-south-digital.png">
+		<img src="logo-simplon.png">
+        </div>
+        <div id="cont2">
+        	<h4>Contact</h4>
+        	<a href="https://www.facebook.com/malik.khelfa.3"><i class="fa fa-facebook-official fa-3x" aria-hidden="true"></i></a>
+        	<a href="https://twitter.com/MalikAliKhelfa"><i class="fa fa-twitter fa-3x" aria-hidden="true"></i></a>
+        	<a href="https://www.linkedin.com/in/malik-khelfa-b725b1127?trk=nav_responsive_tab_profile"><i class="fa fa-linkedin fa-3x" aria-hidden="true"></i></a>
+
+
+        </div>
+
+        </div>
+</body>
